d9f0246f6ed40a6cbcff44ad229e5e1107efa81c
valery.yakovlev <vd.yakovlev@mpoffice.ru>
2019-05-22 15:13:58 +0300

    refactor

---
+++ description-generator/description-generator.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+
+Implement here LSTM to generate descriptions for new product by it's attribute names and values
+Learn on existing corpus of descriptions
+
+@author: valery.yakovlev
+"""
+
+
